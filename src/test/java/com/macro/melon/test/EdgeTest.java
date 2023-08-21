b6fcf2e37c8c411a7c1c66e2044f0641c7e8f51b
DH <psycn@naver.com>
2023-08-21 23:22:55 +0900

    edgeTest 추가

---
+++ src/test/java/com/macro/melon/test/EdgeTest.java
@@ -0,0 +1,45 @@
+package com.macro.melon.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 엣지 연결 테스트
+ */
+public class EdgeTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new EdgeDriver();
+    }
+
+    @AfterEach
+//    void teardown() {
+//        driver.quit();
+//    }
+
+    @Test
+    void test() {
+        // Exercise
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
+
+        // Verify
+        assertThat(title).contains("Hands-On Selenium WebDriver with Java");
+    }
+}
